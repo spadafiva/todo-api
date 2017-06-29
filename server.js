f71dcd7d3d59b5d7a7cc8973961cde792a9265ae
Joe Spadafora <spadafiva@gmail.com>
2017-06-29 00:59:47 -0400

    Adds delete route

--- server.js
+++ server.js
@@ -52,6 +52,22 @@ app.post('/todos', function (req, res) {
 });
 
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+    var id = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: id});
+
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+
+})
+
+
+
 
 app.listen(PORT, function () {
    console.log("Todo api started on port " + PORT);
