f33232f58be281db89ba0597bf67057e386f6359
Joe Spadafora <spadafiva@gmail.com>
2017-06-29 17:28:50 -0400

    Updates production connection to 'process.env.DATABASE_URL'

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
     production: {
         client: 'postgresql',
         connection: {
-            host: 'postgres://jhpohjusbdvrog:1d15ae74a409c2b3ca15115d8ef2e4c5e0b015a590fe5dedd701a98a75bfae6d@ec2-23-23-225-12.compute-1.amazonaws.com:5432/d8n7rjd9t2kabf',
+            host: process.env.DATABASE_URL,
             database: 'sequr'
         }
     }
