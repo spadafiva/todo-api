dd5a27c769497aab072cfd4250131e510eb2cead
Joe Spadafora <spadafiva@gmail.com>
2017-06-28 21:58:55 -0400

    Adds server placeholder

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-/**
- * Created by spadafiva on 6/28/17.
- */
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send("Todo API Root");
+});
+
+app.listen(PORT, function () {
+   console.log("Todo api started on port " + PORT);
+});
