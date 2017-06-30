3ac9af798e3d451be0de299864262300f1a4d310
Joe Spadafora <spadafiva@gmail.com>
2017-06-29 21:02:11 -0400

    Changes knexfile for postgresql connection

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,6 @@ module.exports = {
     },
     production: {
         client: 'postgresql',
-        connection: JSON.parse(process.env.DATABASE_URL)
+        connection: 'postgres://ndzwixkshzeoqm:33efb1fea78baaa21cb6e9b741cf3866dc044df4525b27776ae4db6761fd31cf@ec2-50-19-83-146.compute-1.amazonaws.com:5432/d22su8sr1jv2af'
     }
 }
