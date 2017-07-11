692547efc967df7f628a9885decd3b412d3bb651
Joe Spadafora <spadafiva@gmail.com>
2017-07-10 23:36:08 -0400

    Fixes user migration, adds get/save user

--- migrations/users.js
+++ migrations/users.js
@@ -4,8 +4,8 @@ exports.up = function(knex) {
     return knex.schema
         .createTable('users', function (table) {
             table.increments('id').primary();
-            table.string('description').notNullable();
-            table.boolean('completed').notNullable();
+            table.string('username').notNullable();
+            table.string('password').notNullable();
         });
 };
 
--- server.js
+++ server.js
@@ -12,6 +12,29 @@ app.get('/', function (req, res) {
     res.send("Todo API Root");
 });
 
+// Users
+app.get('/users', function (req, res) {
+    User
+        .fetchAll()
+        .then(function (users) {
+            res.json(users);
+        })
+});
+
+app.post('/users', function (req, res) {
+    new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+        .save()
+        .then(function (saved) {
+            res.json(saved);
+        })
+});
+
+
+// Todo
+
 // GET /todos
 app.get('/todos', function (req, res) {
     var completedQuery = req.query.completed;
